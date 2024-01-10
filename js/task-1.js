c5eafd7e626332c9b44b744df9225721c09440bc
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2024-01-10 19:06:21 +0200

    created new files

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+const listCategory = document.getElementById('categories');
+
+
+const listItem = document.querySelectorAll('.item').length;
+console.log('Number of categories:', listItem);
+
+const items = document.querySelectorAll("#categories .item");
+
+
+items.forEach((item) => {
+    const headerText = item.querySelector("h2").textContent;
+    const numberofElements = item.lastElementChild.children.length;
+
+    console.log('Categories:', headerText);
+    console.log('Elements:', numberofElements);
+});
