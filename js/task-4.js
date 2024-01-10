9589ff8c8e49445d49c171f42f26b47792e58dcb
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2024-01-10 19:17:07 +0200

    created task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    console.log(email.value, password.value);
+    
+     if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+     }
+    const userDetails = { email: email.value, password: password.value }
+
+  console.log(userDetails);
+  event.currentTarget.reset();
+};
 
