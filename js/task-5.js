e1108106a9a9520a32d8ef5f28244a9301d6c33a
Valeri-555 <106805806+Valeri-555@users.noreply.github.com>
2024-01-13 23:18:23 +0200

    update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -2,7 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
 const changeButton = document.querySelector('button.change-color')
 const body = document.querySelector('body')
@@ -10,7 +10,7 @@ const colorValue = document.querySelector('span.color')
 
 
 changeButton.addEventListener('click', e => {
-  const color = getRandomHexColor();
+const color = getRandomHexColor();
   body.style.backgroundColor = color;
   colorValue.textContent = color;
 });
